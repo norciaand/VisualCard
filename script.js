0ca33d5acb11b0b46e9e680e4b64c2017aed3403
andrea <a.norcia06@gmail.com>
2024-06-12 17:26:46 +0200

    www

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let clean;
+let project_title = "Visual Cards"
 
 //PULSANTI
+const header1 = document.getElementById("title");
 const button_n = document.getElementById("button_n");
 const button_plus = document.getElementById("button_plus");
 const button_ref = document.getElementById("button_ref");
 const button_full = document.getElementById("button_full");
+const button_rin = document.getElementById("rin");
 const main = document.getElementById("main");
+
+const colori = ["#ffd0d0", "#d0d1ff", "#d8ffd0", "#fffad0"];
 let arrayCard = [];
 
+
+const isWhitespaceString = str => !str.replace(/\s/g, '').length
+
+button_rin.addEventListener('click',() => {
+    project_title = window.prompt("Rinomina il titolo del Progetto",project_title);
+    if (isWhitespaceString(project_title)){
+        project_title = "Visual Cards"
+    } 
+    header1.textContent = project_title;
+});
+
 function randomColor(){
-    const colori = ["#ffd0d0", "#d0d1ff", "#d8ffd0", "#fffad0"];
+    
     return colori[Math.floor(Math.random() * 4)];
 }
 
-
 button_n.addEventListener('click', () =>{
-    alert("Colonne Attuali: " + button_n.textContent);
+    alert("Note Attuali: " + button_n.textContent);
 });
 
 button_plus.addEventListener('click', () =>{
-
     if (clean){
         main.innerHTML = ""
         clean = false;
         main.style.height = "fit-content";
     }
-
     button_n.textContent++;
-
     let nuovaCard = document.createElement('div');
     nuovaCard.classList.add("card");
     nuovaCard.style.backgroundColor = randomColor();
@@ -42,6 +54,18 @@ button_plus.addEventListener('click', () =>{
 
     arrayCard.push(nuovaCard);
     main.appendChild(nuovaCard);
+
+    nuovaCard.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+        let x = prompt("Inserisci il colore: \n1: Rosso\n2: Blu\n3: Verde\n4: Giallo", "0");
+        if (x > 4 || x < 1){
+            x = randomColor();
+        } else {
+            x = colori[x-1];
+        }
+        nuovaCard.style.backgroundColor = x;
+    }, false);
+
 });
 
 button_ref.addEventListener('click', () =>{
@@ -58,6 +82,7 @@ button_ref.addEventListener('click', () =>{
 });
 
 addEventListener("DOMContentLoaded", () => {
+    header1.textContent = project_title;
     main.style.height = "10vh";
     main.innerHTML = "<h3>PREMI + PER INIZIARE</h3>"
     clean = true;
