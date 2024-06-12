61035486d2ea6c49dfeb7af4c1cd8e469763b01a
andrea <a.norcia06@gmail.com>
2024-06-12 15:34:44 +0200

    ww

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let clean;
 
 //PULSANTI
 const button_n = document.getElementById("button_n");
@@ -14,21 +14,56 @@ function randomColor(){
 }
 
 
+function toggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+        (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+        if (document.documentElement.requestFullScreen) {
+            document.documentElement.requestFullScreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullScreen) {
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
+}
+
 button_n.addEventListener('click', () =>{
     alert("Colonne Attuali: " + button_n.textContent);
-})
+});
 
 button_plus.addEventListener('click', () =>{
+
+    if (clean){
+        main.innerHTML = ""
+        clean = false;
+        main.style.height = "fit-content";
+    }
 
     button_n.textContent++;
 
     let nuovaCard = document.createElement('div');
     nuovaCard.classList.add("card");
     nuovaCard.style.backgroundColor = randomColor();
-    nuovaCard.textContent="x";
+    let titolo = document.createElement('input');
+    titolo.type = "text";  
+    let testo = document.createElement('textarea');
+    testo.cols = 40;
+    testo.rows = 5;
+    nuovaCard.appendChild(titolo);
+    nuovaCard.appendChild(testo);   
+
+
     arrayCard.push(nuovaCard);
     main.appendChild(nuovaCard);
-})
+});
 
 button_ref.addEventListener('click', () =>{
     arrayCard.forEach((item) => {
@@ -38,4 +73,16 @@ button_ref.addEventListener('click', () =>{
     arrayCard.length = 0;
     button_n.textContent=0;
 
-})
+    main.style.height = "10vh";
+    main.innerHTML = "<h3>PREMI + PER INIZIARE</h3>"
+    clean = true;
+});
+
+button_full.addEventListener('click', toggleFullScreen());
+
+
+addEventListener("DOMContentLoaded", () => {
+    main.style.height = "10vh";
+    main.innerHTML = "<h3>PREMI + PER INIZIARE</h3>"
+    clean = true;
+});
