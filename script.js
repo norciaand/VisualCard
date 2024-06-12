ffdc5dfd4be3c6ecc582ff5c431131186634dd3a
andrea <a.norcia06@gmail.com>
2024-06-12 12:23:52 +0200

    ss

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let n = 2;
 const button_n = document.getElementById("button_n");
+const button_plus = document.getElementById("button_plus");
+let arrayCard = []
 
 
 button_n.addEventListener('click', () =>{
     alert("Colonne Attuali: " + button_n.textContent);
+})
+
+button_plus.addEventListener('click', () =>{
+    let nuovaCard = document.createElement('div');
+    
 })
